a4ea595fabb6da3dd719542cb0336983cc062783
kuk329 <kuk329@cau.ac.kr>
2023-12-07 20:23:31 +0900

    feat(baseball): 아웃 구현

--- src/main/java/baseball/sample/Test.java
+++ src/main/java/baseball/sample/Test.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class Test {
     public static void main(String[] args) {
         Referee referee = new Referee();
-        String result = referee.compare(Arrays.asList(3, 1, 2), Arrays.asList(1, 2, 3));
+        String result = referee.compare(Arrays.asList(3, 1, 2), Arrays.asList(5, 8, 9));
         System.out.println(result); // 3스트라이크
     }
 }
