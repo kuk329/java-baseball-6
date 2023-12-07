95451a2615599fcffc94ad2564c86ce132b737e0
kuk329 <kuk329@cau.ac.kr>
2023-12-07 20:10:39 +0900

    test(baseball): Judgement#hasPlace 테스트 코드 작성

--- src/test/java/baseball/sample/domain/JudgmentTest.java
+++ src/test/java/baseball/sample/domain/JudgmentTest.java
@@ -24,5 +24,18 @@ class JudgmentTest {
         Assertions.assertThat(result).isEqualTo(2);
     }
 
+    @DisplayName("컴퓨터 숫자 배열에 같은 위치에 같은 값이 존재하는지를 확인한다.")
+    @Test
+    void hasPlace() {
+        List<Integer> computer = Arrays.asList(1, 2, 3);
+        Judgment judgment = new Judgment();
+
+        boolean result1 = judgment.hasPlace(computer, 0, 1);
+        boolean result2 = judgment.hasPlace(computer, 0, 2);
+
+        Assertions.assertThat(result1).isEqualTo(true);
+        Assertions.assertThat(result2).isEqualTo(false);
+    }
+
 
 }
